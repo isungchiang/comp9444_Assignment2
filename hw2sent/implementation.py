52c858803f76ac8eb0d55d347480687e3d57ae99
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-25 11:02:01 +0000

    Adding tarfile member sanitization to extractall()

--- hw2sent/implementation.py
+++ hw2sent/implementation.py
@@ -24,7 +24,26 @@ def load_data(glove_dict):
     if not os.path.exists(os.path.join(os.path.dirname(__file__), 'data/')):
         with tarfile.open(filename, "r") as tarball:
             dir = os.path.dirname(__file__)
-            tarball.extractall(os.path.join(dir, 'data/'))
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tarball, os.path.join(dir,"data/"))
     dir = os.path.dirname(__file__)
     # read files
     reviews = glob.glob(os.path.join(dir, 'data/pos/*'))
